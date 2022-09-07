421f8edbcc8757391560bdeee41efb5215a43829
Rui Pereira <me@ruipereira.dev>
2022-09-08 00:24:13 +0100

    Build website using pug and webpack

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+
+module.exports = {
+    mode: 'production',
+    entry: './src/css/main.scss',
+    module: {
+        rules: [
+            {
+                test: /\.(scss|css)$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.pug$/,
+                use: ['pug-loader']
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|)$/,
+                type: 'asset',
+                generator: {
+                    filename: 'fonts/[name][ext]'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new RemoveEmptyScriptsPlugin({}),
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].min.css'
+        }),
+        new CopyPlugin({
+            patterns: [
+                {from: './src/img/favicon.ico', to: ''},
+                {from: './src/archive', to: 'archive'},
+                {from: './src/img', to: 'img'}
+            ]
+        }),
+        new HtmlWebpackPlugin({
+            title: 'Rui Pereira',
+            template: './src/index.pug',
+            filename: 'index.html',
+            chunks: []
+        }),
+        new HtmlWebpackPlugin({
+            title: 'Page Not Found',
+            template: './src/404.pug',
+            filename: '404.html',
+            chunks: []
+        })
+    ],
+    optimization: {
+        minimizer: [
+            new CssMinimizerPlugin(), '...'
+        ],
+        minimize: true
+    },
+    output: {
+        clean: true
+    }
+}
