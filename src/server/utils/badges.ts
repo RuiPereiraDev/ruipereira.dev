e941a3785477cffe61b21a48084ba0d26619d60d
Rui Pereira <me@ruipereira.dev>
2024-01-19 23:16:22 +0000

    Reduce cache age to check if it's working properly

--- src/server/utils/badges.ts
+++ src/server/utils/badges.ts
@@ -3,27 +3,27 @@ import type {EventHandler, EventHandlerRequest} from 'h3'
 export const getBStatsPlayers = cachedFunction(async (pluginID: number): Promise<number> => {
     const response = await apiFetch<number[][]>(`https://bstats.org/api/v1/plugins/${pluginID}/charts/players/data?maxElements=1`)
     return response[0][1]
-}, {name: 'bStats/players', getKey: (pluginID) => pluginID, maxAge: 60 * 60, staleMaxAge: -1})
+}, {name: 'bStats/players', getKey: (pluginID) => pluginID, maxAge: 60})
 
 export const getBStatsServers = cachedFunction(async (pluginID: number): Promise<number> => {
     const response = await apiFetch<number[][]>(`https://bstats.org/api/v1/plugins/${pluginID}/charts/servers/data?maxElements=1`)
     return response[0][1]
-}, {name: 'bStats/servers', getKey: (pluginID) => pluginID, maxAge: 60 * 60, staleMaxAge: -1})
+}, {name: 'bStats/servers', getKey: (pluginID) => pluginID, maxAge: 60})
 
 export const getSpigotDownloads = cachedFunction(async (pluginID: number): Promise<number> => {
     const plugin = await apiFetch<SpigotPlugin>(`https://api.spigotmc.org/simple/0.2/index.php?action=getResource&id=${pluginID}`)
     return Number.parseInt(plugin.stats.downloads, 10)
-}, {name: 'spigot/downloads', getKey: (pluginID) => pluginID, maxAge: 60 * 60, staleMaxAge: -1})
+}, {name: 'spigot/downloads', getKey: (pluginID) => pluginID, maxAge: 60})
 
 export const getHangarDownloads = cachedFunction(async (pluginID: number): Promise<number> => {
     const plugin = await apiFetch<HangarPlugin>(`https://hangar.papermc.io/api/v1/projects/${pluginID}`)
     return plugin.stats.downloads
-}, {name: 'hangar/downloads', getKey: (pluginID) => pluginID, maxAge: 60 * 60, staleMaxAge: -1})
+}, {name: 'hangar/downloads', getKey: (pluginID) => pluginID, maxAge: 60})
 
 export const getModrinthDownloads = cachedFunction(async (pluginID: number): Promise<number> => {
     const plugin = await apiFetch<ModrinthProject>(`https://api.modrinth.com/v2/project/${pluginID}`)
     return plugin.downloads
-}, {name: 'modrinth/downloads', getKey: (pluginID) => pluginID, maxAge: 60 * 60, staleMaxAge: -1})
+}, {name: 'modrinth/downloads', getKey: (pluginID) => pluginID, maxAge: 60})
 
 export const getGitHubDownloads = cachedFunction(async (repository: string): Promise<number> => {
     const releases = await apiFetch<GitHubRelease[]>(`https://api.github.com/repos/${repository}/releases`)
@@ -36,17 +36,17 @@ export const getGitHubDownloads = cachedFunction(async (repository: string): Pro
     }
 
     return downloads
-}, {name: 'github/downloads', getKey: (repository) => repository, maxAge: 60 * 60, staleMaxAge: -1})
+}, {name: 'github/downloads', getKey: (repository) => repository, maxAge: 60})
 
 export const getGitHubRelease = cachedFunction(async (repository: string): Promise<string> => {
     const release = await apiFetch<GitHubRelease>(`https://api.github.com/repos/${repository}/releases/latest`)
     return release.name
-}, {name: 'github/release', getKey: (repository) => repository, maxAge: 60 * 60, staleMaxAge: -1})
+}, {name: 'github/release', getKey: (repository) => repository, maxAge: 60})
 
 export const getDiscordOnline = cachedFunction(async (guildID: string): Promise<number> => {
     const guild = await apiFetch<DiscordGuild>(`https://discord.com/api/v6/guilds/${guildID}/widget.json`)
     return guild.presence_count
-}, {name: 'discord/online', getKey: (guildID) => guildID, maxAge: 60 * 60, staleMaxAge: -1})
+}, {name: 'discord/online', getKey: (guildID) => guildID, maxAge: 60})
 
 export const defineBadgeEventHandler = <T extends EventHandlerRequest, D>(
     handler: EventHandler<T, D>
