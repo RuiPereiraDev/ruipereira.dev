38a1ae46b75ab046dbd5b1063d2dfe9339531759
Rui Pereira <me@ruipereira.dev>
2025-09-20 23:16:17 +0100

    Set Cache-Control header for badges and update internal caching times

--- server/utils/badges.ts
+++ server/utils/badges.ts
@@ -13,17 +13,17 @@ export const cachedBStatsServers = defineCachedFunction(async (_: H3Event, plugi
 export const cachedSpigotDownloads = defineCachedFunction(async (_: H3Event, pluginID: number): Promise<number> => {
     const plugin = await apiFetch<SpigotPlugin>(`https://api.spigotmc.org/simple/0.2/index.php?action=getResource&id=${pluginID}`)
     return Number.parseInt(plugin.stats.downloads, 10)
-}, {name: 'spigot/downloads', getKey: (_: H3Event, pluginID: number) => pluginID.toString(), maxAge: 60 * 30})
+}, {name: 'spigot/downloads', getKey: (_: H3Event, pluginID: number) => pluginID.toString(), maxAge: 60 * 60})
 
 export const cachedHangarDownloads = defineCachedFunction(async (_: H3Event, pluginID: number): Promise<number> => {
     const plugin = await apiFetch<HangarPlugin>(`https://hangar.papermc.io/api/v1/projects/${pluginID}`)
     return plugin.stats.downloads
-}, {name: 'hangar/downloads', getKey: (_: H3Event, pluginID: number) => pluginID.toString(), maxAge: 60 * 30})
+}, {name: 'hangar/downloads', getKey: (_: H3Event, pluginID: number) => pluginID.toString(), maxAge: 60 * 60})
 
 export const cachedModrinthDownloads = defineCachedFunction(async (_: H3Event, pluginID: number): Promise<number> => {
     const plugin = await apiFetch<ModrinthProject>(`https://api.modrinth.com/v2/project/${pluginID}`)
     return plugin.downloads
-}, {name: 'modrinth/downloads', getKey: (_: H3Event, pluginID: number) => pluginID.toString(), maxAge: 60 * 30})
+}, {name: 'modrinth/downloads', getKey: (_: H3Event, pluginID: number) => pluginID.toString(), maxAge: 60 * 60})
 
 export const cachedGitHubDownloads = defineCachedFunction(async (_: H3Event, repository: string): Promise<number> => {
     const releases = await apiFetch<GitHubRelease[]>(`https://api.github.com/repos/${repository}/releases`)
@@ -36,12 +36,12 @@ export const cachedGitHubDownloads = defineCachedFunction(async (_: H3Event, rep
     }
 
     return downloads
-}, {name: 'github/downloads', getKey: (_: H3Event, repository: string) => repository, maxAge: 60 * 30})
+}, {name: 'github/downloads', getKey: (_: H3Event, repository: string) => repository, maxAge: 60 * 60})
 
 export const cachedGitHubRelease = defineCachedFunction(async (_: H3Event, repository: string): Promise<string> => {
     const release = await apiFetch<GitHubRelease>(`https://api.github.com/repos/${repository}/releases/latest`)
     return release.name
-}, {name: 'github/release', getKey: (_: H3Event, repository: string) => repository, maxAge: 60 * 30})
+}, {name: 'github/release', getKey: (_: H3Event, repository: string) => repository, maxAge: 60 * 60})
 
 export const cachedDiscordCount = defineCachedFunction(async (_: H3Event, guildID: string) => {
     const data = await apiFetch<DiscordGuild>(`https://discord.com/api/v10/guilds/${guildID}/widget.json`)
@@ -53,7 +53,8 @@ export const defineBadgeEventHandler = <T extends EventHandlerRequest, D>(
 ): EventHandler<T, D> => defineEventHandler<T>(async event => {
     try {
         const response = await handler(event)
-        setHeader(event, 'content-type', 'image/svg+xml')
+        setResponseHeader(event, 'content-type', 'image/svg+xml')
+        setResponseHeader(event, 'cache-control', 'public, must-revalidate, proxy-revalidate, max-age=3600')
         return response
     } catch (err) {
         setResponseStatus(event, 500)
